3d304b74b921f2a41fd0df6af2f253e6eca2eced
= <=>
2025-02-15 13:45:54 +0700

    Add Preview on Products.

--- src/app/products/[id]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function productDetails({params}: {params : {id : string}}){
-    return <h1>Details About Product {params.id}</h1>
-}
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,7 +5,8 @@ export default function productList(){
             <h1>Product 1</h1>
             <h1>Product 2</h1>
             <h1>Product 3</h1>
-            <a href="products/test">Product test</a>
+            <a href="products/test">Product Test</a><br />
+            <a href="products/test/preview/test">Product Test</a>
         </>
     )
 }
