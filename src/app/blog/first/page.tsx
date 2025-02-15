6e4ffb4d426931de2687eb801c0dfd4497234606
= <=>
2025-02-16 00:41:16 +0700

    Adding metadata inside blog for modifying title.

--- src/app/blog/first/page.tsx
+++ src/app/blog/first/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'First Blog',
+}
+
 export default function firstBlog() {
     return <h1>First Page of My Blog</h1>
 }
--- src/app/blog/second/page.tsx
+++ src/app/blog/second/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Second Blog',
+}
+
 export default function secondBlog(){
     return <h1>Second Page of My Blog</h1>
 }
