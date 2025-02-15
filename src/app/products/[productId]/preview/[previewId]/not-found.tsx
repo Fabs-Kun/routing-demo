ea08b20c4109e10fd9c0f9d48b30cfa3325df7aa
= <=>
2025-02-15 21:49:08 +0700

    Adding not found page on preview directory inside products.

---
+++ src/app/products/[productId]/preview/[previewId]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function notFound(){
+    return(
+        <div>
+            <h2>Preview Not Found</h2>
+        </div>
+    )
+}
--- src/app/products/[productId]/preview/[previewId]/page.tsx
+++ src/app/products/[productId]/preview/[previewId]/page.tsx
@@ -1,4 +1,10 @@
+import {notFound} from "next/navigation"
+
 export default function previewDetails({params}: {params: {productId : string; previewId : string;}}){
+    
+    if (parseInt(params.previewId) > 1000) {
+        notFound()
+    }
     return(
         <h1>
             Review {params.previewId} for Product {params.productId}
