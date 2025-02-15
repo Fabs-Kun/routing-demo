b4d3c66dfbb2047db23ad01569df5ebf5003238c
= <=>
2025-02-16 00:18:48 +0700

    Adding metadata inside page on productId in products dir for modifying title with or without async.

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,22 @@
-export default function productDetails({params}: {params:{productId : string}}){
+import { Metadata } from "next";
+
+type props = {
+    params : {
+        productId : string
+    }
+}
+
+export const generateMetadata = async ({params}: props): Promise<Metadata> => {
+    const title = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`iPhone ${params.productId}`)
+        }, 100)
+    })
+    return{
+        title: `Product ${title}`
+    }
+} 
+
+export default function productDetails({params}: props){
     return <h1>Details About Product {params.productId}</h1>
 }
