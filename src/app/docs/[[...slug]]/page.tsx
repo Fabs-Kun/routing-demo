b6f02581683ee7b3e2e24466786cd2212b59cdb4
= <=>
2025-02-16 13:20:41 +0700

    Adding Promise aswell as async and await inside slug in docs to fix .next problem.

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,15 +1,16 @@
-export default function Docs({params}: {params: {slug: string[]}}){
-    if (params.slug?.length === 2){
+export default async function Docs({params}: {params: Promise<{slug: string[]}>}){
+    const slug = (await params).slug
+    if ((await params).slug?.length === 2){
         return(
-            <h1>Viewing Docs for {params.slug[0]} Feature and {params.slug[1]} Concept</h1>
+            <h1>Viewing Docs for {(await params).slug[0]} Feature and {(await params).slug[1]} Concept</h1>
         )
-    } else if (params.slug?.length === 1){
+    } else if ((await params).slug?.length === 1){
         return(
-            <h1>Viewing Docs for {params.slug[0]} Feature</h1>
+            <h1>Viewing Docs for {(await params).slug[0]} Feature</h1>
         )
-    } else if(params.slug?.length === 3){
+    } else if((await params).slug?.length === 3){
         return(
-            <h1>Viewing Docs for {params.slug[0]} Feature , {params.slug[1]} Concept and {params.slug[2]} Example</h1>
+            <h1>Viewing Docs for {(await params).slug[0]} Feature , {(await params).slug[1]} Concept and {(await params).slug[2]} Example</h1>
         )
     } else {
         return(
