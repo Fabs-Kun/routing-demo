40845cee00156b1f72d9914ac9e276f003655904
= <=>
2025-02-16 16:25:31 +0700

    Adding a note in error.tsx for how to make it into nested feature.

--- src/app/products/[productId]/preview/[previewId]/error.tsx
+++ src/app/products/[productId]/preview/[previewId]/error.tsx
@@ -7,3 +7,6 @@ export default function errorBoundary({error, reset}: {error: Error; reset: () =
         </div>
     )
 }
+
+// Note,
+// You can also make this into a nested error handling, by simply just move this file into products directory, which is the root for everything connected with product.
