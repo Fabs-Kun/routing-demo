190308f4ce485edd30d7c9c94affb621c1e230e1
= <=>
2025-02-16 11:35:20 +0700

    Adding link to connect between home and blog page.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Link from "next/link"
 
 export const metadata: Metadata = {
     title: {
@@ -7,5 +8,16 @@ export const metadata: Metadata = {
 }
 
 export default function Blog(){
-    return <h1>Here's My Blog</h1>
+    return(
+        <>  
+            <Link href="/">Home</Link>
+            <h1>Here's My Blog</h1>
+            <h2>
+                <Link href="/blog/first">First Page</Link>
+            </h2>
+            <h2>
+                <Link href="/blog/second">Second Page</Link>
+            </h2>
+        </>
+    )
 }
