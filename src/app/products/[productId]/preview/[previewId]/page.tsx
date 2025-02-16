04a0e392195fbd235cfd685e8b420f302c9470b4
= <=>
2025-02-16 13:04:05 +0700

    Adding error massage on development build.

--- src/app/products/[productId]/preview/[previewId]/page.tsx
+++ src/app/products/[productId]/preview/[previewId]/page.tsx
@@ -1,6 +1,15 @@
 import {notFound} from "next/navigation"
 
+function getRandomInt(count:number) {
+    return Math.floor(Math.random() * count);
+}
+
 export default function previewDetails({params}: {params: {productId : string; previewId : string;}}){
+    const random = getRandomInt(2);
+
+    if(random === 1){
+        throw new Error("Error loading preview...")
+    }
     
     if (parseInt(params.previewId) > 1000) {
         notFound()
