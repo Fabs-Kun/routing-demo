7b1f2fccfca9f364517688fb7aa5271f7833c3fa
= <=>
2025-02-16 11:21:42 +0700

    Adding new code inside page.

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -6,7 +6,7 @@ export default function productList(){
             <h1>Product 2</h1>
             <h1>Product 3</h1>
             <a href="products/test">Product Test</a><br />
-            <a href="products/test/preview/test">Product Test</a>
+            <a href="products/test/preview/test">Product Test Preview</a>
         </>
     )
 }
