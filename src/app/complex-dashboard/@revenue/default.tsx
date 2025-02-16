646735629ecc6b4166c436c8ea9af7162b0701ed
= <=>
2025-02-17 00:04:14 +0700

    Adding default.tsx in every slots except notifications and children in complex-dashboard to fix unmatch route reload error.

---
+++ src/app/complex-dashboard/@revenue/default.tsx
@@ -0,0 +1,5 @@
+import Card from "@/components/card"
+
+export default function defaultRevenueMetrics(){
+    return <Card>Revenue Metrics</Card>
+}
---
+++ src/app/complex-dashboard/default.tsx
@@ -0,0 +1,3 @@
+export default function defaultComplexDashboardPage(){
+    return <h1>Complex Dashboard</h1>
+}
